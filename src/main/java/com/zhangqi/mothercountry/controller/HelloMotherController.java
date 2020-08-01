6ee8810044a313c66b0d63b4f6c561dadd451d8c
zhangqi234110 <51742008+zhangqi234110@users.noreply.github.com>
2020-08-01 11:00:01 +0800

    Update HelloMotherController.java

--- src/main/java/com/zhangqi/mothercountry/controller/HelloMotherController.java
+++ src/main/java/com/zhangqi/mothercountry/controller/HelloMotherController.java
@@ -37,7 +37,7 @@ public class HelloMotherController {
         map.put("message","这里是妈妈们交流的圣地！测试一下");
         map.put("ss","是的撒旦撒旦");
         map.put("token",token);
-        map.put("sdasd","v的覅哦");
+        map.put("实现","12313");
         return map;
     }
     @RequestMapping("/ewr")
